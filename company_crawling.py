0762f9a348b7b5acf093152d34970a127712b40c
moon4656 <moon4656@gmail.com>
2024-08-22 11:48:02 +0900

    기업 웹크로링 최초

---
+++ company_crawling.py
@@ -0,0 +1,59 @@
+
+
+import urllib3
+import json
+import pandas as pd
+import psycopg2
+import requests
+import json
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+
+company_id = '100'
+category = 'A01'
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+response = requests.get(
+            url='https://www.bigdata-map.kr/api/board/faq?search.page=1&search.rows=10&search.keyword=', 
+            headers={
+                    'user-agent' : 'Mozilla 5.0', 
+                    'Referer':'https://www.bigdata-map.kr/board/faq'
+                    }, 
+                verify=False)
+
+# 응답이 성공적인지 확인
+if response.status_code == 200:
+    # JSON 데이터 파싱
+    # print(response.text)
+    bs = BeautifulSoup(response.text, 'lxml')    
+else:
+    print(f"Failed to retrieve data: {response.status_code}")
+
+items = json.loads(response.text).get('list')  
+# print(items)
+
+for item in items:
+    question = item['title']
+    answer   = item['content']
+    create_dt = item['uploadTs']
+    update_dt = item['updateTs']
+    
+    # print(company_id, category, question, answer, create_dt, update_dt )
+    
+    with psycopg2.connect(
+        host='localhost',
+        dbname='postgres',
+        user='postgres',
+        password='1234',
+        port=5432,
+    ) as conn:
+        with conn.cursor() as cur:
+              cur.execute(f"""INSERT INTO question ( company_id,
+                                        category,
+                                        question,
+                                        answer,
+                                        create_dt,
+                                        update_dt ) VALUES ('{company_id}', '{category}', '{question}', '{answer}', '{create_dt}', '{update_dt}')""")
+
+
