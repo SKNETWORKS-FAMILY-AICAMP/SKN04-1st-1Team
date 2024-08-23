f5e08d6c7894e3a0ed13f4bdf5b74faf70bba2d2
kohyourim <gusgmltnr6811@gmail.com>
2024-08-23 10:05:40 +0900

    streamlit 초안작성

---
+++ streamlit/Intro.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import time
+import datetime
+from PIL import Image
+import requests
+
+
+st.set_page_config(
+  page_title="SKN4th_team1",
+  page_icon=":car:"
+)
+
+st.title('SKN04-1st-1Team')
+st.write('고유림, 김문수, 김정훈, 김효은')
+
+st.divider()
---
+++ streamlit/pages/1_전국 자동차 등록 현황.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import time
+import datetime
+
+
+st.title('전국 자동차 등록 현황')
+
