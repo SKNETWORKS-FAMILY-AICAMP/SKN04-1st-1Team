fd162cdd63019af608b4be4a6cce0e70ad11dfc8
jeonhun6084 <jeonhun6084@gmail.com>
2024-08-23 15:59:51 +0900

    에러로 인한 파라미터 수정

--- streamlit/pages/1_전국 자동차 등록 현황.py
+++ streamlit/pages/1_전국 자동차 등록 현황.py
@@ -8,9 +8,9 @@ conn = st.connection("postgresql", type="sql")
 df = conn.query('SELECT * FROM car_reg_info;', ttl="10m")
 
 st.subheader('등록대수')
-st.bar_chart(df, use_container_width=True, x="year", y="register_cnt", x_label='연도', y_label='등록대수(만대)')
+st.bar_chart(df, use_container_width=True, x="year", y="register_cnt")
 
 st.divider()
 
 st.subheader('전년대비 증가대수(천대)')
-st.line_chart(df, x="year", y="last_increase_cnt", x_label='연도', y_label='전년대비 증가대수(천대)')
+st.line_chart(df, x="year", y="last_increase_cnt")
